5bf1f7718060ea8f981edbc5768fcafb3c7e1ec4
NataliaDiazm <112571171+NataliaDiazm@users.noreply.github.com>
2026-05-26 21:37:27 -0500

    arreglo cors

--- src/main/java/com/cohorte11/primerProyecto/config/SecurityConfig.java
+++ src/main/java/com/cohorte11/primerProyecto/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
 
         http
     .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configure(http))
+                .cors(cors -> {})
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
